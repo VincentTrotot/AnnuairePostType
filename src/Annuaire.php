58883a8c8aca744719316ff0598f27ff007b9539
Vincent Trotot <vincent.trotot@gmail.com>
2021-06-26 22:54:07 +0200

    FEAT: ajout des sub_categories
    Ajout de la propriété statique sub_categories, un tableau de toutes les sous-categories des fiches d'annuaires instanciées

--- src/Annuaire.php
+++ src/Annuaire.php
@@ -10,6 +10,8 @@ class Annuaire extends Post
     public $address;
     public $phone;
     public $mail;
+    
+    public static $sub_categories = [];
 
     public function __construct($pid = null)
     {
@@ -18,5 +20,19 @@ class Annuaire extends Post
         $this->address = $this->meta('vt_annuaire_address');
         $this->phone = $this->meta('vt_annuaire_phone');
         $this->mail = $this->meta('vt_annuaire_mail');
+        
+        $sub_categories = $this->terms([
+            'query' => [
+                'taxonomy' =>'vt_annuaire_sub_category',
+                'orderby' => 'title',
+                'order' => 'ASC'
+            ]
+        ]);
+        
+        foreach ($sub_categories as $sub_categorie) {
+            if (!in_array($sub_categorie, self::$sub_categories)) {
+               self::$sub_categories[] = $sub_categorie;
+           }
+        }
     }
 }
